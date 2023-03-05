7c4b8c487a293c4e40572370d9ad8ffe366f3fe3
Ron Gonzales <gonzrona@isu.edu>
2023-03-04 20:21:53 -0700

    worked on interest function

--- interest_functions.py
+++ interest_functions.py
@@ -34,14 +34,34 @@ def apr_monthly_compound(principal:float, payment:float, apr:float) -> int:
         year = Year(current_year(), next_month()) if year_count==0 else Year(current_year() + year_count)
         daily_apr = apr/365 if not year.leap else apr/366
         for month in year:
-            interest = 0
-            for _ in range(month.days):
-                # principal += principal*daily_apr
-                interest += principal*daily_apr
-            principal = principal + interest - payment
+            principal +=  month.days*principal*daily_apr - payment
             payment_count += 1
             if principal <= 0:
                 run = False
                 break
         year_count += 1
     return payment_count
+
+
+def  apr_daily_compound_accrue(principal:float, payment:float, apr:float) -> float:
+    """ computes the total interest over the remainder of the loan """
+    run = True
+    year_count = 0
+    # payment_count = 0
+    total_interest = 0
+    if payment < 31*principal*apr/365:
+        print("Payment too small to pay off loan")
+        return 0
+    while run:
+        year = Year(current_year(), next_month()) if year_count==0 else Year(current_year() + year_count)
+        daily_apr = apr/365 if not year.leap else apr/366
+        for month in year:
+            month_interest = month.days*principal*daily_apr
+            principal += month_interest - payment
+            total_interest += month_interest
+            # payment_count += 1
+            if principal <= 0:
+                run = False
+                break
+        year_count += 1
+    return total_interest
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from interest_functions import apr_daily_compound, apr_monthly_compound
 print(apr_daily_compound(10000.0, 200.0, .0423))
 print(apr_daily_compound(10000.0, 200.0, .2049))
 print(apr_monthly_compound(10000.0, 200.0, .2049))
+print(apr_daily_compound_accrue(80000.0, 600, .0423))
